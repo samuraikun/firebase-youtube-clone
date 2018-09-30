c4042756842f96c9f1ed0a0153ca6399e5b3adc8
samuraikun <tazitaziawawa@gmail.com>
2018-10-01 01:40:14 +0900

    mp4動画に限り、Videoモデルのサブコレクション作成

--- functions/index.js
+++ functions/index.js
@@ -11,3 +11,17 @@ if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'transcodeVideo'
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'saveUser') {
   exports.saveUser = require('./saveUser').saveUser;
 }
+
+/*
+* Triggers when was created new video data, video data is copied Firestore
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'onUsersVideoCreate') {
+  exports.onUsersVideoCreate = require('./copyVideoMetadata').onUsersVideoCreate;
+}
+
+/*
+* Triggers when was updated new video data, video data is copied Firestore
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'onUsersVideoUpdate') {
+  exports.onUsersVideoUpdate = require('./copyVideoMetadata').onUsersVideoUpdate;
+}
