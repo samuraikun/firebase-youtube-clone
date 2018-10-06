831a501df2b34b105f67ad18a9260431d472a338
samuraikun <tazitaziawawa@gmail.com>
2018-10-07 03:06:42 +0900

    Email・Password による認証でもユーザー情報を保存できるようにする
    save hosting cache

--- functions/saveUser.js
+++ functions/saveUser.js
@@ -11,18 +11,20 @@ try {
   console.log(error);
 }
 
+const defaultUserIcon = 'https://firebasestorage.googleapis.com/v0/b/fir-clone-1a266.appspot.com/o/default-user-icons%2F1.png?alt=media&token=b5e090f4-d2b1-4544-92bd-379a3d22b470'
+
 exports.saveUser = functions.auth.user().onCreate(async user => {
   try {
     const result = await admin.firestore().doc(`users/${user.uid}`).create({
       uid: user.uid,
-      displayName: user.displayName,
+      displayName: user.displayName || '名無しさん',
       email: user.email,
       emailVerified: user.emailVerified,
-      photoURL: user.photoURL,
+      photoURL: user.photoURL || defaultUserIcon,
       phoneNumber: user.phoneNumber,
       providerData: {
-        providerId: user.providerData[0].providerId,
-        uid: user.providerData[0].uid
+        providerId: user.providerData.length === 0 ? 'password' : user.providerData[0].providerId,
+        uid: user.providerData.length === 0 ? user.email : user.providerData[0].uid
       },
       disabled: user.disabled
     });
