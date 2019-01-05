28c6e59e42b5b7fd5ec3c6252f405fdcdcff5654
samuraikun <tazitaziawawa@gmail.com>
2019-01-06 01:06:46 +0900

    Firebase の設定を１つにまとめる

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import config from '../config/firebase-config';
 import Header from './Header';
 import VideoUpload from './VideoUpload';
 import VideoFeed from './VideoFeed';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    // Initialize Firebase
-    firebase.initializeApp(config);
-    firebase.firestore().settings({ timestampsInSnapshots: true });
-  }
-
   render() {
     return (
       <Router>
--- src/components/VideoFeed.js
+++ src/components/VideoFeed.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import firebase from 'firebase/app';
+import firebase from '../config/firebase';
 import 'firebase/firestore';
 import VideoPlayer from './VideoPlayer';
 
