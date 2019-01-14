80991de3acb4db5efefc7a4ebe2afa94f0363eca
samuraikun <tazitaziawawa@gmail.com>
2019-01-14 14:16:33 +0900

    Redux とコンポーネントを接続

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import VideoUpload from './VideoUpload';
 import VideoFeed from './VideoFeed';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+    rootElement
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 registerServiceWorker();
